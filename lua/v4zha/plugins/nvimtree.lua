6a2d93be005d712c1025a462a2e1c8d36849c5e5
v4zha <arvin_j4848@protonmail.com>
2023-02-02 12:27:01 +0530

    nvim-tree-open

--- lua/v4zha/plugins/nvimtree.lua
+++ lua/v4zha/plugins/nvimtree.lua
@@ -1,83 +1,108 @@
 local status, nvim_tree = pcall(require, "nvim-tree")
 if not status then
-    return
+  return
 end
 
 -- from LunarVim config : )
 
 local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
 if not config_status_ok then
-    return
+  return
 end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
-    open_on_setup = true,
-    open_on_setup_file = false,
-    actions = {
-        open_file = {
-            quit_on_open = true
-        }
-    },
-    sync_root_with_cwd = true,
-    respect_buf_cwd = true,
-    update_focused_file = {
-        enable = true,
-        update_root = true
-    },
-    renderer = {
-        root_folder_modifier = ":t",
-        icons = {
-            glyphs = {
-                default = "",
-                symlink = "",
-                folder = {
-                    arrow_open = "",
-                    arrow_closed = "",
-                    default = "",
-                    open = "",
-                    empty = "",
-                    empty_open = "",
-                    symlink = "",
-                    symlink_open = ""
-                },
-                git = {
-                    unstaged = "",
-                    staged = "S",
-                    unmerged = "",
-                    renamed = "➜",
-                    untracked = "U",
-                    deleted = "",
-                    ignored = "◌"
-                }
-            }
-        }
-    },
-    diagnostics = {
-        enable = true,
-        show_on_dirs = true,
-        icons = {
-            hint = "",
-            info = "",
-            warning = "",
-            error = ""
-        }
-    },
-    view = {
-        width = 30,
-        side = "left",
-        mappings = {
-            list = { {
-                key = { "l", "<CR>", "o" },
-                cb = tree_cb "edit"
-            }, {
-                key = "h",
-                cb = tree_cb "close_node"
-            }, {
-                key = "v",
-                cb = tree_cb "vsplit"
-            } }
-        }
+  -- open_on_setup = true,
+  -- open_on_setup_file = false,
+  actions = {
+    open_file = {
+      quit_on_open = true
     }
+  },
+  sync_root_with_cwd = true,
+  respect_buf_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_root = true
+  },
+  renderer = {
+    root_folder_modifier = ":t",
+    icons = {
+      glyphs = {
+        default = "",
+        symlink = "",
+        folder = {
+          arrow_open = "",
+          arrow_closed = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = ""
+        },
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "U",
+          deleted = "",
+          ignored = "◌"
+        }
+      }
+    }
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = ""
+    }
+  },
+  view = {
+    width = 30,
+    side = "left",
+    mappings = {
+      list = { {
+        key = { "l", "<CR>", "o" },
+        cb = tree_cb "edit"
+      }, {
+        key = "h",
+        cb = tree_cb "close_node"
+      }, {
+        key = "v",
+        cb = tree_cb "vsplit"
+      } }
+    }
+  }
 }
+
+local function open_nvim_tree()
+
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not no_name and not directory then
+    return
+  end
+
+
+  -- change to the directory
+  if directory then
+    vim.cmd.cd(data.file)
+  end
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
